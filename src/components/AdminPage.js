de71db664205a8c51aa10238e880f34109758ac2
shuva9800 <ffshuva@gmail.com>
2024-08-22 15:58:51 +0530

    just

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -4,8 +4,6 @@ import React from 'react';
 import '../App.css';
 import PhaserGame from './PhaserGame';
 import io from 'socket.io-client';
-import { Link } from 'react-router-dom';
-
 const socket = io('https://gameappbackend.onrender.com'); // Connect to the Socket.io server
 
 
