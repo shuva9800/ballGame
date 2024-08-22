59f05411091dd07e9c1b6ccddb6809e68bc7fa71
shuva9800 <ffshuva@gmail.com>
2024-08-23 00:31:04 +0530

    once again

--- src/components/PhaserGame.js
+++ src/components/PhaserGame.js
@@ -1,12 +1,114 @@
 
 
+// import React, { useEffect } from 'react';
+// import Phaser from 'phaser';
+// import io from 'socket.io-client';
+
+// const socket = io('https://gameappbackend.onrender.com'); // Connect to the Socket.io server
+// // const socket = io('http://localhost:4000'); // Connect to the Socket.io server
+
+
+// function PhaserGame() {
+//     useEffect(() => {
+//         const config = {
+//             type: Phaser.AUTO,
+//             width: 400,
+//             height: 400,
+//             backgroundColor: '#87CEEB',
+//             parent: 'phaser-game',
+//             physics: {
+//                 default: 'arcade',
+//                 arcade: {
+//                     gravity: { y: 0 },
+//                     debug: false,
+//                 },
+//             },
+//             scene: {
+//                 preload: preload,
+//                 create: create,
+//                 update: update,
+//             },
+//         };
+
+//         const game = new Phaser.Game(config);
+//         let ball;
+
+//         function preload() {
+//             this.load.image('ball', 'ball.svg');
+//         }
+
+//         function create() {
+//             ball = this.physics.add.image(200, 200, 'ball');
+//             ball.setCollideWorldBounds(true);
+//             ball.setBounce(1, 1);
+//             ball.setVelocity(200, 200);
+//         }
+
+//         function update() {
+//             // No additional logic needed here for now
+//         }
+
+//         PhaserGame.moveBall = function (direction) {
+//             const speed = 200;
+//             switch (direction) {
+//                 case 'top-left':
+//                     ball.setPosition(50, 50);
+//                     ball.setVelocity(-speed, -speed);
+//                     break;
+//                 case 'top-right':
+//                     ball.setPosition(350, 50);
+//                     ball.setVelocity(speed, -speed);
+//                     break;
+//                 case 'bottom-left':
+//                     ball.setPosition(50, 350);
+//                     ball.setVelocity(-speed, speed);
+//                     break;
+//                 case 'bottom-right':
+//                     ball.setVelocity(speed, speed);
+//                     break;
+//                 case 'left-top':
+//                     ball.setVelocity(-speed, -speed / 2);
+//                     break;
+//                 case 'left-bottom':
+//                     ball.setVelocity(-speed, speed / 2);
+//                     break;
+//                 case 'right-top':
+//                     ball.setVelocity(speed, -speed / 2);
+//                     break;
+//                 case 'right-bottom':
+//                     ball.setVelocity(speed, speed / 2);
+//                     break;
+//                 default:
+//                     ball.setVelocity(0, 0);
+//                     break;
+//             }
+//         };
+
+//         // Listen for button press events from the server
+        
+        
+//         socket.on('buttonPress', (direction) => {
+//             PhaserGame.moveBall(direction);
+//         });
+
+//         return () => {
+//             game.destroy(true);
+//         };
+//     }, []);
+
+//     return <div id="phaser-game"></div>;
+// }
+
+// export default PhaserGame;
+
+
+
 import React, { useEffect } from 'react';
 import Phaser from 'phaser';
 import io from 'socket.io-client';
 
-const socket = io('https://gameappbackend.onrender.com'); // Connect to the Socket.io server
-// const socket = io('http://localhost:4000'); // Connect to the Socket.io server
-
+// Ensure socket is created only once
+const socket = io('https://gameappbackend.onrender.com')
 
 function PhaserGame() {
     useEffect(() => {
@@ -41,51 +143,12 @@ function PhaserGame() {
             ball = this.physics.add.image(200, 200, 'ball');
             ball.setCollideWorldBounds(true);
             ball.setBounce(1, 1);
-            ball.setVelocity(200, 200);
         }
 
         function update() {
             // No additional logic needed here for now
         }
 
-        // PhaserGame.moveBall = function (direction) {
-        //     const speed = 200;
-        //     switch (direction) {
-        //         case 'top-left':
-        //             ball.setPosition(50, 50);
-        //             ball.setVelocity(-speed, -speed);
-        //             break;
-        //         case 'top-right':
-        //             ball.setPosition(350, 50);
-        //             ball.setVelocity(speed, -speed);
-        //             break;
-        //         case 'bottom-left':
-        //             ball.setPosition(50, 350);
-        //             ball.setVelocity(-speed, speed);
-        //             break;
-        //         case 'bottom-right':
-        //             ball.setVelocity(speed, speed);
-        //             break;
-        //         case 'left-top':
-        //             ball.setVelocity(-speed, -speed / 2);
-        //             break;
-        //         case 'left-bottom':
-        //             ball.setVelocity(-speed, speed / 2);
-        //             break;
-        //         case 'right-top':
-        //             ball.setVelocity(speed, -speed / 2);
-        //             break;
-        //         case 'right-bottom':
-        //             ball.setVelocity(speed, speed / 2);
-        //             break;
-        //         default:
-        //             ball.setVelocity(0, 0);
-        //             break;
-        //     }
-        // };
-
-        // Listen for button press events from the server
-        
         PhaserGame.moveBall = function (direction) {
             const speed = 300;
             switch (direction) {
@@ -126,14 +189,15 @@ function PhaserGame() {
                     break;
             }
         };
-        
-        
+
+        // Ensure this listener is added only once
         socket.on('buttonPress', (direction) => {
             PhaserGame.moveBall(direction);
         });
 
         return () => {
             game.destroy(true);
+               socket.off('buttonPress'); // Cleanup event listener
         };
     }, []);
 
@@ -141,107 +205,3 @@ function PhaserGame() {
 }
 
 export default PhaserGame;
-
-
-
-// import React, { useEffect } from 'react';
-// import Phaser from 'phaser';
-// import io from 'socket.io-client';
-
-// // Ensure socket is created only once
-// const socket = io('https://gameappbackend.onrender.com')
-
-// function PhaserGame() {
-//     useEffect(() => {
-//         const config = {
-//             type: Phaser.AUTO,
-//             width: 400,
-//             height: 400,
-//             backgroundColor: '#87CEEB',
-//             parent: 'phaser-game',
-//             physics: {
-//                 default: 'arcade',
-//                 arcade: {
-//                     gravity: { y: 0 },
-//                     debug: false,
-//                 },
-//             },
-//             scene: {
-//                 preload: preload,
-//                 create: create,
-//                 update: update,
-//             },
-//         };
-
-//         const game = new Phaser.Game(config);
-//         let ball;
-
-//         function preload() {
-//             this.load.image('ball', 'ball.svg');
-//         }
-
-//         function create() {
-//             ball = this.physics.add.image(200, 200, 'ball');
-//             ball.setCollideWorldBounds(true);
-//             ball.setBounce(1, 1);
-//         }
-
-//         function update() {
-//             // No additional logic needed here for now
-//         }
-
-//         PhaserGame.moveBall = function (direction) {
-//             const speed = 300;
-//             switch (direction) {
-//                 case 'top-left':
-//                     ball.setPosition(50, 50);
-//                     ball.setVelocity(-speed, -speed);
-//                     break;
-//                 case 'top-right':
-//                     ball.setPosition(350, 50);
-//                     ball.setVelocity(speed, -speed);
-//                     break;
-//                 case 'bottom-left':
-//                     ball.setPosition(50, 350);
-//                     ball.setVelocity(-speed, speed);
-//                     break;
-//                 case 'bottom-right':
-//                     ball.setPosition(350, 350);
-//                     ball.setVelocity(speed, speed);
-//                     break;
-//                 case 'left-top':
-//                     ball.setPosition(50, 50);
-//                     ball.setVelocity(-speed, -speed / 2);
-//                     break;
-//                 case 'left-bottom':
-//                     ball.setPosition(50, 350);
-//                     ball.setVelocity(-speed, speed / 2);
-//                     break;
-//                 case 'right-top':
-//                     ball.setPosition(350, 50);
-//                     ball.setVelocity(speed, -speed / 2);
-//                     break;
-//                 case 'right-bottom':
-//                     ball.setPosition(350, 350);
-//                     ball.setVelocity(speed, speed / 2);
-//                     break;
-//                 default:
-//                     ball.setVelocity(0, 0);
-//                     break;
-//             }
-//         };
-
-//         // Ensure this listener is added only once
-//         socket.on('buttonPress', (direction) => {
-//             PhaserGame.moveBall(direction);
-//         });
-
-//         return () => {
-//             game.destroy(true);
-//         };
-//     }, []);
-
-//     return <div id="phaser-game"></div>;
-// }
-
-// export default PhaserGame;
